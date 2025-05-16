66950ea03c2204646c3c570b2d05710be122da93
Tõnis Ormisson <tonis@andmemasin.eu>
2025-05-16 15:11:32 +0300

    added removePrefixFromRules to RuleHelper, release 3.2.1

--- src/RuleHelper.php
+++ src/RuleHelper.php
@@ -27,4 +27,27 @@ class RuleHelper
 
     }
 
+
+    /**
+     * Removes a specific table prefix from any field in conditions
+     */
+    public static function removePrefixFromRules(Rule $rules, string $prefix) : Rule
+    {
+        if(count($rules->children) === 0) {
+            return $rules;
+        }
+
+        foreach ($rules->children as $key => $child) {
+            if ($child->condition !== null) {
+                $rules->children[$key] = static::removePrefixFromRules($child, $prefix);
+            } else {
+                if(!is_null($child->field) && str_contains($child->field, "$prefix.")) {
+                    $child->field = str_replace("$prefix.", "", $child->field);
+                }
+            }
+        }
+        return $rules;
+    }
+
+
 }
